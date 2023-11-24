c56ee87852fc3bc9885a3df74d551d6e7e653f59
Claire Fisher <cafisher83@yahoo.co.uk>
2023-11-24 18:06:33 +0000

    fix: jquery installation issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 console.log("IT WORKS!");
 
 $("p").click(function () {
+  console.log("djgbz");
   $("p").css("background-color", "red");
 });
 
 // $("p").click(function () {
 //   $("p").css("color", "blue");
+// });
+
+// $(document).ready(function () {
+//   $("p").click(function () {
+//     $("p").css("background-color", "red");
+//   });
 // });
